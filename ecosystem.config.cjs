6fbfddc7eb53fb66bed87007c253da5def6d35a9
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2026-05-02 19:47:27 +0530

    fixes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,16 @@
 module.exports = {
   apps: [{
     name: 'peter-website',
-    script: 'npx',
-    args: 'serve dist -l 3000',
-    cwd: '/home/ubuntu/peter-website',
+    script: 'npm',
+    args: 'start',
+    cwd: '/home/ubuntu/pm-course',  // ← fix this
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      PORT: 3000
     },
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '200M'
+    max_memory_restart: '500M'
   }]
 };

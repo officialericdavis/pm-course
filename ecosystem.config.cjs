ffe849d8c739649c2c14237fdc445d922cb0a901
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2026-05-02 19:35:39 +0530

    hosting

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,16 @@
 module.exports = {
   apps: [{
-    name: 'peter-website',
-    script: 'npx',
-    args: 'serve dist -l 3000',
+    name: 'your-app',
+    script: 'npm',
+    args: 'start',
     cwd: '/home/ubuntu/peter-website',
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      PORT: 3000
     },
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '200M'
+    max_memory_restart: '500M'
   }]
 };
